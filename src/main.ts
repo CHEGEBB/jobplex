b0a9a7ddff3125e671cc27b40aceb2225846757c
CHEGEBB <chegephil24@gmail.com>
2025-04-14 09:37:28 +0300

    Refactor font imports and add Font Awesome icons

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
-import '@fortawesome/fontawesome-free/css/solid.min.css';
-import '@fortawesome/fontawesome-free/css/regular.min.css';
-import '@fortawesome/fontawesome-free/css/brands.min.css';
+// import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+// import '@fortawesome/fontawesome-free/css/solid.min.css';
+// import '@fortawesome/fontawesome-free/css/regular.min.css';
+// import '@fortawesome/fontawesome-free/css/brands.min.css';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
